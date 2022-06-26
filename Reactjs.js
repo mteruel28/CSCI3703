fa82abdffcd6b141b531c27112d19a72270c3627
mteruel28 <miguel.teruel98@gmail.com>
2022-06-26 05:59:37 -0500

    ReactJSnotes

---
+++ Reactjs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import",/index.css";
+
+ReactDOM.render(
+    React.createElement("h1",null,"Hello"),
+    document.getElementById("root")
+);
