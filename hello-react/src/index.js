a5f6f90d94a1008be423ba96b94ea9f5b7e7c4a0
mteruel28 <miguel.teruel98@gmail.com>
2022-06-26 14:12:59 -0500

    update index

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -6,11 +6,11 @@ ReactDOM.render(
   React.createElement("h1",null,"Hello!"),
   document.getElementById("root")
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+ // <React.StrictMode>
+    //<App />
+ /// </React.StrictMode>
+//);
 
 
