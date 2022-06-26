977cb7a223b1e8eec6e123c4eb347efbcddee4bd
mteruel28 <106028705+mteruel28@users.noreply.github.com>
2022-06-26 15:30:55 -0500

    Update index.js

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function Hello() {
+  return (<div>
+ <h1> Welcome to React!</h1>
+ <p> Onto Better Things!</p>
+  </div>
+  )
+}
+
 let city = { 
   name: "New York",
   country: "United States"};
 
 ReactDOM.render(
- <h1>{city.name} is in {city.country}</h1>>
+  <><Hello />
+  <h1 id="heading" className="cool-text">
+    {city.name} is in {city.country}
+  </h1></>>
+
  document.getElementById("root")
 );
+
 
 
 
