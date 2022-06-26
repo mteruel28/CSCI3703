2d9ec248b605cdb0aeb18396baf2addaa137c22e
mteruel28 <miguel.teruel98@gmail.com>
2022-06-26 14:11:35 -0500

    update index

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1",null,"Hello!")
-)
+  React.createElement("h1",null,"Hello!"),
+  document.getElementById("root")
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
