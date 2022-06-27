8d1101c05dc363a38d226363df1fa55dae36ead0
Miguel <miguel.teruel98@gmail.com>
2022-06-27 06:34:42 -0500

    updates

---
+++ JavaScriptnotes.js
@@ -0,0 +1,41 @@
+/*Creating an Object 
+
+const pool = {
+name: pool
+volume: 30 ,
+color: "blue" ,
+width: 45
+height: 35
+}
+*/
+
+/* Creating a Class , very similar context to java
+class Name{} this declares a class
+const Name = class{} expression for class
+
+example:
+class Pool{
+    constructor // contains parameters and properties
+    ( width
+    height
+    volume
+    )
+}
+*/
+
+/* mixing strings and literals
+const content ``; - backticks under escape key, tells browser that template literal, mix html and string with JS expressions
+
+${pool} - insert JS expression,  calls JS expressions, render then output to placeholder
+affects line 3
+*/
+
+/*DOM: Document Object Model
+document.querySelector("brown dog") - selects element that matches CSS selectors
+
+modify classes for elements
+document.querySelector()classList.add() - adds classes
+document.querySelector()classList.remove() - removes class
+document.querySelector()classList.toggle() - toggles between add and remove, toggles between classes
+document.querySelector()classList.replace()-changes classes based on user
+*/
